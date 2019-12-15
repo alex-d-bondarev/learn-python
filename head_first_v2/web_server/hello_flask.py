23dd4d284598ed2f4abc840d2bb2e121438e3acf
Alex <alex.d.bondarev@gmail.com>
2019-12-15 15:07:26 +0200

    finish chapter 5

--- head_first_v2/web_server/hello_flask.py
+++ head_first_v2/web_server/hello_flask.py
@@ -1,17 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from lsearch import search_for_letters
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title='Welcome to search for letters on the web!)')
 
 
-@app.route('/search')
-def do_search() -> str:
-    return str(search_for_letters(letters='eiru!'))
+@app.route('/search', methods=['POST'])
+def do_search() -> 'html':
+    title = 'Here are the results:'
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search_for_letters(phrase, letters))
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
+else:
+    app.run()
