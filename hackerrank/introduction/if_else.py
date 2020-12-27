22740d8898b2f170eda0186e4ea0f2d0921d39a7
Alex <alex.d.bondarev@gmail.com>
2020-12-28 00:20:21 +0200

    add basic hackerrunk solutions

---
+++ hackerrank/introduction/if_else.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+
+def even(number):
+    return number % 2 == 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    weird = "Weird"
+    not_weird = "Not Weird"
+
+    if not even(n):
+        output = weird
+    else:
+        if 6 <= n <= 20:
+            output = weird
+        else:
+            output = not_weird
+
+    print(output)
