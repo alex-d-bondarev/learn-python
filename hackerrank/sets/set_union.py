0691cee7f33fa715d8cab916f38a7ad69bb9f5e4
Alex <alex.d.bondarev@gmail.com>
2021-01-26 23:31:30 +0200

    add set_union solution

---
+++ hackerrank/sets/set_union.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    english_subscribers_amount = input()
+    english_subscribers = set(map(int, input().split()))
+    french_subscribers_amount = input()
+    french_subscribers = set(map(int, input().split()))
+
+    print(len(english_subscribers.union(french_subscribers)))
