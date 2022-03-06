ac63741c7b9158a94d5dd758a21ba7864c35420b
Alex <alex.d.bondarev@gmail.com>
2022-03-06 19:47:51 +0000

    extract docker commands

--- experiments/resiliency_test_urls.py
+++ experiments/resiliency_test_urls.py
@@ -4,6 +4,7 @@ import traceback
 import docker
 import requests
 from docker.errors import NotFound
+from docker.models.containers import Container
 from requests import ReadTimeout
 from requests.exceptions import ConnectionError
 from timeout_decorator import timeout_decorator
@@ -35,6 +36,81 @@ class RipperSetup(DockerSetup):
         self.command = "www." + test_url.replace("http://", "").replace("https://", "")
 
 
+class DockerWrapper(object):
+    """Move docker methods into a separate class"""
+
+    def __init__(self, docker_setup: DockerSetup):
+        self.docker_setup = docker_setup
+        self.container_id = None
+
+    def run_container_attached(self) -> str:
+        """ Run docker container and return final decoded logs
+
+        :return:
+        """
+        client = self._get_docker_client()
+
+        return client.containers.run(
+            image=self.docker_setup.get_image(),
+            tty=True,
+            command=self.docker_setup.get_command(),
+            detach=False,
+            remove=True,
+        ).decode("utf-8")
+
+    @staticmethod
+    def _get_docker_client():
+        return docker.from_env()
+
+    def run_docker_detached(self) -> str:
+        """ Run detached docker container and return its id
+
+        :return:
+        """
+        client = self._get_docker_client()
+
+        self.container_id = client.containers.run(
+            image=self.docker_setup.get_image(),
+            tty=True,
+            command=self.docker_setup.get_command(),
+            detach=True,
+            remove=True,
+        ).id
+
+        return self.container_id
+
+    def get_detached_container_logs(self) -> str:
+        """ Get current logs of container per its id
+
+        :return:
+        """
+        container = self.get_container()
+        container_logs = container.logs().decode('utf-8')
+        return container_logs
+
+    def get_container(self) -> Container:
+        """ Self evident
+
+        :param docker_id:
+        :return:
+        """
+        return self._get_docker_client().containers.get(self.container_id)
+
+    def container_is_running(self) -> bool:
+        """ Self evident
+
+        :return:
+        """
+        return self.get_container() is not None
+
+    def stop(self) -> bool:
+        """ Self evident
+
+        :return:
+        """
+        return self.get_container().stop()
+
+
 def start_the_infinite_resiliency_test(duration=300):
     """
     Load test all urls from file if they are active
@@ -55,22 +131,20 @@ def start_the_infinite_resiliency_test(duration=300):
 def _load_test_the_url(test_url, duration):
     try:
         requests.get(test_url, timeout=30)
-        _run_bombardier(test_url, duration)
+        _load_test_the_url_with_700s_timeout(test_url, duration)
 
     except (ConnectionError, ReadTimeout) as e:
         print(f"{test_url} is already down")
-        print(traceback.format_exc())
 
-    except StopIteration as e:
+    except TimeoutError as e:
         print(f"{test_url} load test took over 700 seconds")
-        print(traceback.format_exc())
 
     except Exception as e:
         print(f"failed to process {test_url}")
         print(traceback.format_exc())
 
 
-def _print_container_log(log: str):
+def _print_bombardier_stats(log: str):
     print_line = False
     text_to_print = ""
     lines = log.split("\n")
@@ -84,7 +158,7 @@ def _print_container_log(log: str):
             text_to_print += line + "\n"
 
 
-def _run_docker(test_url: str, duration: int):
+def _run_the_test(test_url: str, duration: int):
     start = time.time()
     do_ripper = True
     ripper_setup = RipperSetup(
@@ -94,69 +168,45 @@ def _run_docker(test_url: str, duration: int):
     )
 
     print(f"Start ripper for {test_url}")
-    client = docker.from_env()
-    docker_id = client.containers.run(
-        image=ripper_setup.get_image(),
-        tty=True,
-        command=ripper_setup.get_command(),
-        detach=True,
-        remove=True,
-    ).id
-    container = None
+    docker_wrapper = DockerWrapper(ripper_setup)
+    docker_wrapper.run_docker_detached()
 
     try:
         while time.time() - start < duration and do_ripper:
             time.sleep(10)
-            container = docker.from_env().containers.get(docker_id)
-            container_logs = container.logs().decode('utf-8')
+            container_logs = docker_wrapper.get_detached_container_logs()
             count_rippering = container_logs.count("rippering")
             count_down = container_logs.count("down")
 
             if count_down / count_rippering > 0.5:
                 do_ripper = False
 
-        if container is not None:
-            docker.from_env().containers.get(docker_id).stop()
+        if docker_wrapper.container_is_running():
+            docker_wrapper.stop()
 
         print(f"Stopped ripper for {test_url}")
 
     except NotFound as e:
         print(f"Failed to ripper {test_url}")
 
-        # new_duration = duration - (time.time() - start) - 1
-        # if new_duration > 1:
-        #     print(f"Start bombardier for {test_url}")
-        #     bombardier_setup = BombardierSetup(
-        #         duration=int(new_duration),
-        #         request_timeout=30,
-        #         test_url=test_url,
-        #     )
-        #
-        #     decoded_container_log = run_container_and_wait_for_logs(bombardier_setup)
-        #     _print_container_log(decoded_container_log)
+        new_duration = duration - (time.time() - start) - 1
+
+        if new_duration > 1:
+            print(f"Start bombardier for {test_url}")
+            bombardier_setup = BombardierSetup(
+                duration=int(new_duration),
+                request_timeout=30,
+                test_url=test_url,
+            )
+
+            decoded_container_log = DockerWrapper(bombardier_setup).run_container_attached()
+            _print_bombardier_stats(decoded_container_log)
 
 
-def run_container_and_wait_for_logs(docker_setup: DockerSetup):
-    """ Run docker command and return decoded logs
-
-    :param docker_setup:
-    :return:
-    """
-    client = docker.from_env()
-
-    return client.containers.run(
-        image=docker_setup.get_image(),
-        tty=True,
-        command=docker_setup.get_command(),
-        detach=False,
-        remove=True,
-    ).decode("utf-8")
-
-
-@timeout_decorator.timeout(700, timeout_exception=StopIteration)
-def _run_bombardier(test_url, duration):
+@timeout_decorator.timeout(700, timeout_exception=TimeoutError)
+def _load_test_the_url_with_700s_timeout(test_url, duration):
     print(f"Start Testing {test_url}")
-    _run_docker(test_url, duration)
+    _run_the_test(test_url, duration)
     print(f"Finished testing {test_url}")
 
 
