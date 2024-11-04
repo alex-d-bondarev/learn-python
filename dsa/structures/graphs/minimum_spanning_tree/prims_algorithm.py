c1090a57a2f486e2304f46965baad43f8b09c3df
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2024-11-04 19:16:56 +0000

    Put all prims_alg code in one place

--- dsa/structures/graphs/minimum_spanning_tree/prims_algorithm.py
+++ dsa/structures/graphs/minimum_spanning_tree/prims_algorithm.py
@@ -1,15 +1,32 @@
 from collections import namedtuple
 from heapq import heappop, heappush
 
-from dsa.structures.graphs.adjacency_matrix_undirected_with_weight import (
-    MyAdjMatrUnDirWeightedGraph,
-)
 from dsa.structures.graphs.minimum_spanning_tree.mst import MST
 
 Visitor = namedtuple("Visitor", ["position", "parent"])
+Neighbor = namedtuple("Neighbor", ["position", "weight"])
 
 
-def find_mst(graph: MyAdjMatrUnDirWeightedGraph) -> MST:
+class MatrixGraph:
+    def __init__(self, size):
+        self.matrix = [[None] * size for _ in range(size)]
+        self.size = size
+
+    def add_edge(self, first, second, weight) -> "MatrixGraph":
+        if 0 <= first < self.size and 0 <= second < self.size:
+            self.matrix[first][second] = weight
+            self.matrix[second][first] = weight
+        return self
+
+    def get_neighbors(self, index) -> list[Neighbor]:
+        return [
+            Neighbor(position=position, weight=weight)
+            for position, weight in enumerate(self.matrix[index])
+            if weight
+        ]
+
+
+def find_mst(graph: MatrixGraph) -> MST:
     """
 
     Some pretty bad docstring
@@ -30,19 +47,21 @@ def find_mst(graph: MyAdjMatrUnDirWeightedGraph) -> MST:
 
                 for neighbor in graph.get_neighbors(visitor.position):
                     if not visited[neighbor.position]:
-                        heappush(to_visit, (
-                            neighbor.weight,
-                            Visitor(position=neighbor.position, parent=visitor.position),
-                        ))
+                        to_visit = _add_visitor(neighbor, to_visit, visitor)
     return MST(parents=parents, weights=weights)
 
 
+def _add_visitor(neighbor, to_visit, visitor):
+    visitor = Visitor(position=neighbor.position, parent=visitor.position)
+    heappush(to_visit, (neighbor.weight, visitor))
+    return to_visit
+
+
 def test_prim_mst():
-    graph = MyAdjMatrUnDirWeightedGraph(6)
+    graph = MatrixGraph(6)
 
     (
-        graph
-        .add_edge(0, 1, 1)
+        graph.add_edge(0, 1, 1)
         .add_edge(0, 2, 8)
         .add_edge(0, 4, 6)
         .add_edge(1, 3, 2)
@@ -58,11 +77,10 @@ def test_prim_mst():
 
 
 def test_w3s_example():
-    graph = MyAdjMatrUnDirWeightedGraph(8)
+    graph = MatrixGraph(8)
 
     (
-        graph
-        .add_edge(0, 1, 4)
+        graph.add_edge(0, 1, 4)
         .add_edge(0, 3, 3)
         .add_edge(1, 2, 3)
         .add_edge(1, 3, 5)
